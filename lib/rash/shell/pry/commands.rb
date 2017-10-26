a67735a3580c145c0dc58b1d8678e411d5f76f3c
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-10-26 15:58:51 +0200

    Corrected RuboCop offences.

--- lib/rash/shell/pry/commands.rb
+++ lib/rash/shell/pry/commands.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 require 'rash/shell/pry/commands/h'
 require 'rash/shell/pry/commands/no_prompt'
--- lib/rash/shell/pry/commands/no_prompt.rb
+++ lib/rash/shell/pry/commands/no_prompt.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Pry::Commands.create_command 'no-prompt' do
   description 'Show/hide REPL command prompt prefix'
 
