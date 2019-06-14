9c50f7adafa18d79efce128e94403a2b94a05aeb
Jean Mertz <jean@mertz.fm>
2019-06-14 08:33:29 +0200

    fix dangling reference to renamed variable

--- lib/rash/shell/pry/prompt.rb
+++ lib/rash/shell/pry/prompt.rb
@@ -2,14 +2,14 @@
 
 Pry.config.prompt = [
   proc do |obj, _, pry|
-    target = obj.to_s == 'main' ? '' : " (#{target_self.to_s.split('::').last})"
+    target = obj.to_s == 'main' ? '' : " (#{obj.to_s.split('::').last})"
     env = Rash::Shell.environment_with_color
 
     "#{pry.config.prompt_name} [#{env}]#{target} % "
   end,
 
   proc do |obj, _, pry|
-    target = obj.to_s == 'main' ? '' : " (#{target_self.to_s.split('::').last})"
+    target = obj.to_s == 'main' ? '' : " (#{obj.to_s.split('::').last})"
     env = Rash::Shell.environment_with_color
 
     "#{pry.config.prompt_name} [#{env}]#{target} * "
